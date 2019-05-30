00f06641108ededa72997a8c51e92bbc3ef8f8d7
Sushmitharguptha <sushmitharguptha@gmail.com>
2019-05-30 16:44:53 +0530

    employee model

---
+++ DemoGit/src/Employee.java
@@ -0,0 +1,25 @@
+
+public class Employee {
+private int empid;
+private String empName;
+private int sal;
+public int getEmpid() {
+	return empid;
+}
+public void setEmpid(int empid) {
+	this.empid = empid;
+}
+public String getEmpName() {
+	return empName;
+}
+public void setEmpName(String empName) {
+	this.empName = empName;
+}
+public int getSal() {
+	return sal;
+}
+public void setSal(int sal) {
+	this.sal = sal;
+}
+
+}
